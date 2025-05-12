08acc5f7808286cef5dcf6580a850a422a5a4a62
Jolvera2001 <jolvera6@stedwards.edu>
2025-05-11 23:42:49 -0500

    starting a core plugins to start off simple

---
+++ src/main/kotlin/Core/CorePlugin.kt
@@ -0,0 +1,13 @@
+package Core
+
+import PluginSystem.IPlugin
+
+class CorePlugin: IPlugin {
+    override fun onInitialize() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDisable() {
+        TODO("Not yet implemented")
+    }
+}
