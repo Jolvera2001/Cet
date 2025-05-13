4b73fe4fc575b12a8b79f1f085baa3149f9aac0e
Jolvera2001 <jolvera6@stedwards.edu>
2025-05-12 23:41:12 -0500

    started tests with the plugin system

--- src/test/kotlin/PluginSystemTests.kt
+++ src/test/kotlin/PluginSystemTests.kt
@@ -1,8 +1,21 @@
+import PluginSystem.PluginSystem
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 
 class PluginSystemTests : FunSpec({
-    test("first test") {
-        1 + 3 shouldBe 4
+    lateinit var pluginSystem: PluginSystem
+
+    beforeTest {
+        pluginSystem = PluginSystem()
+    }
+
+    afterTest {
+        pluginSystem.stop()
+    }
+
+    context("plugin system lifecycle related tests") {
+        test("plugin system starts without being null") {
+            pluginSystem shouldNotBe null
+        }
     }
 })
