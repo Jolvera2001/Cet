21dd30e19d7bb7b013b292deb6b499aac745bbff
Jolvera2001 <jolvera6@stedwards.edu>
2025-05-14 22:55:06 -0500

    added a small implementation on how plugins should start up including a scope argument

--- src/main/kotlin/Core/CorePlugin.kt
+++ src/main/kotlin/Core/CorePlugin.kt
@@ -2,13 +2,25 @@ package Core
 
 import PluginSystem.EventHandler
 import PluginSystem.IPlugin
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlin.coroutines.CoroutineContext
 
-class CorePlugin: IPlugin {
+class CorePlugin(context: CoroutineContext) : IPlugin {
+    private val scope = CoroutineScope(context)
+
     override fun onInitialize(eventHandler: EventHandler) {
-        TODO("Not yet implemented")
+        eventHandler.Subscribe().onEach { event ->
+            when (event) {
+                // TODO: define events
+            }
+        }
+            .launchIn(scope)
     }
 
     override fun onDisable() {
-        TODO("Not yet implemented")
+        scope.cancel()
     }
 }
--- src/main/kotlin/PluginSystem/IPlugin.kt
+++ src/main/kotlin/PluginSystem/IPlugin.kt
@@ -1,5 +1,7 @@
 package PluginSystem
 
+import kotlin.coroutines.CoroutineContext
+
 interface IPlugin {
     fun onInitialize(eventHandler: EventHandler)
     fun onDisable()
