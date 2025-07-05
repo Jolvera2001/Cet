66720b22e69163fadd5104b46dfcf2dedc6a3786
Jolvera2001 <jolvera6@stedwards.edu>
2025-07-05 14:00:11 -0500

    implemented parsing through menuitems and some small styling stuff

--- app/src/main/kotlin/core/ui/TopBar.kt
+++ app/src/main/kotlin/core/ui/TopBar.kt
@@ -10,16 +10,23 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.MenuDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import sharedItems.SubMenuItem
 
 @Preview
@@ -28,7 +35,7 @@ fun TopBar(menuItems: Map<String, List<SubMenuItem>>) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .height(28.dp)
+            .height(30.dp)
             .background(MaterialTheme.colorScheme.surfaceDim),
     ) {
         LazyRow {
@@ -44,11 +51,19 @@ fun Dropdown(menuName: String, menuItems: List<SubMenuItem>) {
     var expanded by remember { mutableStateOf(false) }
 
     Box {
-        TextButton(onClick = { expanded = true }) {
-            Text(menuName)
+        TextButton(
+            onClick = { expanded = true },
+            shape = RectangleShape,
+        ) {
+            Text(
+                menuName,
+                style = MaterialTheme.typography.labelMedium,
+            )
         }
         DropdownMenu(
             expanded = expanded,
+            offset = DpOffset(0.dp, 0.dp),
+            shape = MenuDefaults.shape,
             onDismissRequest = { expanded = false }
         ) {
             menuItems.forEach { item ->
